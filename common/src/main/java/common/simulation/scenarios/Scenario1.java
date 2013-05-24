d71786df61526702ca27e10303352ccc4b9f2b49
Nubsis <nubsis@gmail.com>
2013-05-24 13:24:07 +0200

    Add more nodes into the scenario (changed from 20 to 50)

--- common/src/main/java/common/simulation/scenarios/Scenario1.java
+++ common/src/main/java/common/simulation/scenarios/Scenario1.java
@@ -4,42 +4,42 @@ import se.sics.kompics.p2p.experiment.dsl.SimulationScenario;
 
 @SuppressWarnings("serial")
 public class Scenario1 extends Scenario {
-	private static SimulationScenario scenario = new SimulationScenario() {{
-                
-		StochasticProcess process0 = new StochasticProcess() {{
-			eventInterArrivalTime(constant(100));
-			raise(20, Operations.peerJoin(), 
-                                uniform(0, Integer.MAX_VALUE)
-                             );
-		}};
-                
-		StochasticProcess process1 = new StochasticProcess() {{
-			eventInterArrivalTime(constant(100));
-			raise(20, Operations.peerJoin(), 
-                                uniform(0, Integer.MAX_VALUE)
-                                );
-		}};
-		
-		StochasticProcess process2 = new StochasticProcess() {{
-			eventInterArrivalTime(constant(100));
-			raise(100, Operations.addIndexEntry(), 
-                                uniform(0, Integer.MAX_VALUE)
-                                );
-		}};
-        
-        StochasticProcess process3 = new StochasticProcess() {{
-           eventInterArrivalTime(constant(100));
-           raise(1, Operations.peerFail, uniform(1, 1));
-        }};
+    private static SimulationScenario scenario = new SimulationScenario() {{
 
-		process0.start();
-		process1.startAfterTerminationOf(25000, process0);
-		process2.startAfterTerminationOf(2000, process1);
-        //process3.startAfterTerminationOf(6000, process2);
+	StochasticProcess process0 = new StochasticProcess() {{
+	    eventInterArrivalTime(constant(100));
+	    raise(50, Operations.peerJoin(), 
+		    uniform(0, Integer.MAX_VALUE)
+		    );
 	}};
 
-	// -------------------------------------------------------------------
-	public Scenario1() {
-		super(scenario);
-	}
+	StochasticProcess process1 = new StochasticProcess() {{
+	    eventInterArrivalTime(constant(100));
+	    raise(20, Operations.peerJoin(), 
+		    uniform(0, Integer.MAX_VALUE)
+		    );
+	}};
+
+	StochasticProcess process2 = new StochasticProcess() {{
+	    eventInterArrivalTime(constant(100));
+	    raise(100, Operations.addIndexEntry(), 
+		    uniform(0, Integer.MAX_VALUE)
+		    );
+	}};
+
+	StochasticProcess process3 = new StochasticProcess() {{
+	    eventInterArrivalTime(constant(100));
+	    raise(1, Operations.peerFail, uniform(1, 1));
+	}};
+
+	process0.start();
+	process1.startAfterTerminationOf(25000, process0);
+	process2.startAfterTerminationOf(2000, process1);
+	//process3.startAfterTerminationOf(6000, process2);
+    }};
+
+    // -------------------------------------------------------------------
+    public Scenario1() {
+	super(scenario);
+    }
 }
